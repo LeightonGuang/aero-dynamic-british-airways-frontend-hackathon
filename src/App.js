a7e1d9795fb913d0d2d2f62060182105dde51a24
Leighton Guang <leighton.guang@gmail.com>
2023-11-17 13:54:32 +0000

    merge leighton-branch to develop

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import ResultPage from "./pages/ResultPage/ResultPage";
 import Header from "./components/Header/Header";
 import LandingPage from "./pages/LandingPage/LandingPage";
+import FormPage from "./pages/FormPage/FormPage";
+import ResultPage from "./pages/ResultPage/ResultPage";
 import TestPage from "./pages/TestPageAfterLanding/TestPage";
+import "./App.scss";
 
 export default function App() {
   return (
@@ -10,9 +12,9 @@ export default function App() {
       <BrowserRouter>
         <Header />
         <Routes>
-
           <Route path="/" element={<LandingPage />} />
-  <Route path="/results" element={<ResultPage />} />
+          <Route path="/form" element={<FormPage />} />
+          <Route path="/results" element={<ResultPage />} />
           <Route path="/form" element={<TestPage />} />
         </Routes>
       </BrowserRouter>
---
+++ src/pages/FormPage/FormPage.jsx
@@ -0,0 +1,26 @@
+import CalendarCard from "../../components/CalendarCard/CalendarCard";
+import FlightDurationCard from "../../components/FlightDurationCard/FlightDurationCard";
+import WeatherCard from "../../components/WeatherCard/WeatherCard";
+import "./FormPage.scss";
+
+export default function FormPage() {
+  return (
+    <div className="result">
+      <div className="result__container">
+        <CalendarCard />
+
+        <div className="result__title-container">
+          <h1 className="result__title">Tell us more about your flight</h1>
+          <p className="result__title-description">
+            Select at least 3 options from each category
+          </p>
+        </div>
+
+        <div className="result__option-container">
+          <FlightDurationCard />
+          <WeatherCard />
+        </div>
+      </div>
+    </div>
+  );
+}
