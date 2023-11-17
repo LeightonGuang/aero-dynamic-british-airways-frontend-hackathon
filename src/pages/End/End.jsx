dcee00624442454aa612cf21a40653b987fe9c33
Olusanya Jegede <olusanyajegede1@gmail.com>
2023-11-17 15:26:34 +0000

    Fix: end page

--- src/pages/End/End.jsx
+++ src/pages/End/End.jsx
@@ -5,6 +5,11 @@ const End = () => {
     <>
       <div className="end">
         <h1 className="end__title">Thank you!</h1>
+        <img
+          className="end__image"
+          src="https://images.unsplash.com/photo-1626803566136-5e867cce462e?q=80&w=2500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt=""
+        />
       </div>
     </>
   );
