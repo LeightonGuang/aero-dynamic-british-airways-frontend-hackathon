05ed65cc69cf0f748389067ab7ff2ad45f7b6d36
Leighton Guang <leighton.guang@gmail.com>
2023-11-17 11:31:50 +0000

    fix: weather title

--- src/components/WeatherCard/WeatherCard.jsx
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -3,10 +3,10 @@ export default function WeatherCard() {
   return (
     <div className="weather">
       <div className="weather__container">
-        <h2 className="weather__title">Flight Duration</h2>
+        <h2 className="weather__title">Weather</h2>
         <div className="weather__button-container">
           <button className="weather__button">Tropical</button>
-          <button className="weather__button">Option 2</button>
+          <button className="weather__button">City</button>
           <button className="weather__button">Option 3</button>
         </div>
       </div>
