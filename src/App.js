9486ba4f32fb8dab219652db8f6632aef1f7d6f8
Leighton Guang <leighton.guang@gmail.com>
2023-11-17 11:20:07 +0000

    feat: add result page path

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ResultPage from "./pages/ResultPage/ResultPage";
 
 export default function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route />
+          <Route path="/result" element={<ResultPage />} />
         </Routes>
       </BrowserRouter>
     </div>
