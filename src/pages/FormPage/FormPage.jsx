7471c49a58ccf5c0316f38d3136f90fd9a716206
Leighton Guang <leighton.guang@gmail.com>
2023-11-17 15:14:26 +0000

    refactor: use option card component

--- src/pages/FormPage/FormPage.jsx
+++ src/pages/FormPage/FormPage.jsx
@@ -1,26 +1,65 @@
 import CalendarCard from "../../components/CalendarCard/CalendarCard";
-import FlightDurationCard from "../../components/FlightDurationCard/FlightDurationCard";
-import WeatherCard from "../../components/WeatherCard/WeatherCard";
+import OptionCard from "../../components/OptionCard/OptionCard";
 import "./FormPage.scss";
 import { Link } from "react-router-dom";
 import "../ProfilePage/ProfilePage.scss";
 
 export default function FormPage() {
   return (
-    <div className="result">
-      <div className="result__container">
+    <div className="form">
+      <div className="form__container">
         <CalendarCard />
 
-        <div className="result__title-container">
-          <h1 className="result__title">Tell us more about your flight</h1>
-          <p className="result__title-description">
+        <div className="form__title-container">
+          <h1 className="form__title">Tell us more about your flight</h1>
+          <p className="form__title-description">
             Select at least 3 options from each category
           </p>
         </div>
 
-        <div className="result__option-container">
-          <FlightDurationCard />
-          <WeatherCard />
+        <div className="form__option-container">
+          <OptionCard
+            topic="Flight Duration"
+            options={[
+              "Quick Getaway (1-3 hours)",
+              "Midday Escapade (4-6 hours)",
+              "Epic Expedition (7+ hours)",
+            ]}
+          />
+          <OptionCard
+            topic="Weather Wonderland"
+            options={["Sun Seeker", "Mild Marvel", "Winter Wonderland"]}
+          />
+          <OptionCard
+            topic="City Chic or Remote Retreat"
+            options={[
+              "Culinary Connoisseur",
+              "Art Admirer",
+              "Sports Enthusiast",
+              "Nature Lover",
+              "Music Maestro",
+              "Tech Explorer",
+            ]}
+          />
+          <OptionCard
+            topic="Flight Connection Conundrum"
+            options={[
+              "City Slicker",
+              "Countryside Charmer",
+              "Remote Bliss",
+              "Urban Explorer",
+              "Beach Bliss",
+              "Adventure Aficionado",
+            ]}
+          />
+          <OptionCard
+            topic="Passionate Pursuits"
+            options={[
+              "Direct Diva",
+              "Layover Lover",
+              "Surprise Me! (Mix of both)",
+            ]}
+          />
         </div>
 
         <div>
