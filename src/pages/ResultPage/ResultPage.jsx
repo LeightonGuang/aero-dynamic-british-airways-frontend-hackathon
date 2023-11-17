741c9edae6cc7b80a742c4ba3ca11ad79a032452
Zeid Bsaibes <zeid.bsaibes@gmail.com>
2023-11-17 15:33:56 +0000

    added scroll to top on results

--- src/pages/ResultPage/ResultPage.jsx
+++ src/pages/ResultPage/ResultPage.jsx
@@ -1,7 +1,20 @@
+import { useEffect } from "react";
 import SuggestionCard from "../../components/SuggestionCard/SuggestionCard";
 import "./ResultPage.scss";
 
 const ResultPage = () => {
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
+  };
+
+  useEffect(() => {
+    scrollToTop();
+  }, []);
+
   return (
     <div className="results">
       <section>
